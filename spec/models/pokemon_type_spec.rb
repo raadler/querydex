615aa3e2ff88e464f489a0e9c5b5bc4cd75e8c0f
Edward Loveall <edward@edwardloveall.com>
2016-12-14 09:57:39 -0500

    Add tests for Pokemon and Type associations
    These should have come before the code. My bad.
    
    I also added some pretty funky indentation and formatting to appease rubocop. I'm not sure I agree with the rules, but right now getting less warnings with more working code is a good thing.

--- spec/models/pokemon_type_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe PokemonType do
-  it { should have_valid(:pokemon_id).when(2) }
-  it { should_not have_valid(:pokemon_id).when(nil, '') }
-
-  it { should have_valid(:type_id).when(2) }
-  it { should_not have_valid(:type_id).when(nil, '') }
-end
--- spec/models/type_spec.rb
+++ spec/models/type_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Type, type: :model do
+  describe 'associations' do
+    it do
+      should have_many(:primary_pokemons).class_name('Pokemon')
+        .with_foreign_key('type_1_id')
+    end
+    it do
+      should have_many(:secondary_pokemons).class_name('Pokemon')
+        .with_foreign_key('type_2_id')
+    end
+  end
+
   it { should have_valid(:name).when('Ghost') }
   it { should_not have_valid(:name).when(nil, '') }
 
