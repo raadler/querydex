0a24e388541d347dece0d1b7ffacbe6d299324db
Rachel Adler <adler.rachel.a@gmail.com>
2017-06-27 13:30:51 -0400

    add csv and seeding for types, remove hex colors, because who has time for that

--- spec/models/type_spec.rb
+++ spec/models/type_spec.rb
@@ -14,7 +14,4 @@ RSpec.describe Type, type: :model do
 
   it { should have_valid(:name).when('Ghost') }
   it { should_not have_valid(:name).when(nil, '') }
-
-  it { should have_valid(:color).when('#8e77b0') }
-  it { should_not have_valid(:name).when(nil, '') }
 end
