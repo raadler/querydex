74cc3e4d5b5d6e4e34b07a9183c8d2db31cd7e16
Rachel Adler <adler.rachel.a@gmail.com>
2017-07-07 10:03:30 -0400

    removed PokemonTypes model, which is no longer necessary

--- app/models/pokemon_type.rb
+++
@@ -1,8 +0,0 @@
-# Join table for Pokemon and Type
-
-class PokemonType < ActiveRecord::Base
-  validates :pokemon_id, presence: true
-  validates :type_id, presence: true
-  belongs_to :pokemon
-  belongs_to :type
-end
