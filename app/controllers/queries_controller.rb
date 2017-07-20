95f3685cc532269ff5dcff7c23b56e680ccd1ef0
Rachel Adler <adler.rachel.a@gmail.com>
2017-07-20 15:12:34 -0400

    Fixing the queries bugs, making views
    It turns out I had a typo in the view name, and needed to have it link to the x_path, which I did by changing the name in the queries array.
    I also finished a draft of the views for the first 3 queries. I'll probably go back and add more information to the views, but this is enough for now.

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,23 +1,22 @@
 class QueriesController < ApplicationController
   def find_single_pokemon_by_name
-    @pokemon = Pokemon.where(name: "Charizard")
+    @pokemon = Pokemon.find_by(name: "Charizard")
   end
   def list_all_pokemon_by_name_and_ndex
-    @pokemons = Pokemon.all.select("name, ndex").order(:ndex)
+    @pokemons = Pokemon.all.select("name, ndex").order(:ndex).to_a
   end
   def find_all_pokemon_of_type_1_and_type_2
     # Pokemon.where("type_1_id = 7 AND type_2_id = 3")
     type_1 = Type.find_by(name: 'Bug')
     type_2 = Type.find_by(name: 'Flying')
-    @pokemons = Pokemon.where(type_1: type_1, type_2: type_2)
+    @pokemons = Pokemon.where(type_1: type_1, type_2: type_2).to_a
   end
   def index
     @queries = {
-      "Find a pokemon by name" => find_single_pokemon_by_name,
-      "List pokemon by name with national pokedex id" => list_all_pokemon_by_name_and_ndex,
-      "Find pokemon by types" => find_all_pokemon_of_type_1_and_type_2
+      "Find a pokemon by name" => find_single_pokemon_by_name_path,
+      "List pokemon by name with national pokedex id" => list_all_pokemon_by_name_and_ndex_path,
+      "Find pokemon by types" => find_all_pokemon_of_type_1_and_type_2_path
     }
-
   end
   def show
   end
