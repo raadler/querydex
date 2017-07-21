b0b365db87468c868cd0953a6069249fc6d50ec5
Rachel Adler <adler.rachel.a@gmail.com>
2017-07-21 12:38:09 -0400

    Query 5: Find the easiest to catch Pokemon
    Catch Rate goes from 0-255, with 0 being the hardest to catch and 255 being the easiest. I was to show all the easiest to catch ones in the event of a tie. There are many. MANY.

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/list_all_pokemon_by_name_and_ndex', to: 'queries#list_all_pokemon_by_name_and_ndex'
   get '/find_all_pokemon_of_type_1_and_type_2', to: 'queries#find_all_pokemon_of_type_1_and_type_2'
   get '/find_all_pokemon_of_type_1_or_type_2', to: 'queries#find_all_pokemon_of_type_1_or_type_2'
+  get '/easiest_to_catch', to: 'queries#easiest_to_catch'
 end
